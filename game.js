1f3697745e18d248d39d31f4c57b9f90fa52be7a
andrepn <Andrew_Noel@live.com>
2019-05-16 13:52:34 -0700

    add render function for paddl class

--- game.js
+++ game.js
@@ -11,7 +11,8 @@ class Paddle {
   //Initialize a new Paddle
     //vertical_length and horizontal_length are the dimensions of new paddle
     //initial position is the percentage of total distance the paddle is from the top
-  constructor(vertical_length, horizontal_length, position) {
+  constructor(player_id, vertical_length, horizontal_length, position) {
+    this.player_id = player_id;
     this.vertical_length = vertical_length;
     this.horizontal_length = horizontal_length;
     this.position = position;
@@ -20,4 +21,13 @@ class Paddle {
   move(percentage_move) {
     this.position = this.position - percentage_move;
   }
+
+  render() {
+    //access html element of the paddle and update everything to be this object
+    var element = document.querySelectorAll(".paddle[data-player='" + this.player_id + "']")[0];
+    return element;
+  }
 }
+
+var player_one_paddle = new Paddle(1, 10, 5, 50);
+var player_two_paddle = new Paddle(2, 10, 5, 50);
